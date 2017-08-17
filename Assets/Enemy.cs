86ccc25c7a98b6b13bc13b0ad50ce8ab2d75088d
Victor <victor.badoiu@eandm.ro>
2017-08-17 17:26:03 +0300

    Implement enemy GFX
    Implemented enemy GFX in blender
    Implemented movement animation and die animation.
    Fixed bugs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,12 +7,28 @@ public class Enemy : MonoBehaviour {
     public Rigidbody player;
     float bounceAmount = 6f;
     public Transform deathParticles;
+    public Animator enemyAnim;
+    private Animator centerAnim;
+    public Transform colliders;
 
+    private void Awake()
+    {
+        centerAnim = transform.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        
+    }
     public void Die ()
     {
         player.velocity = new Vector3(player.velocity.x, bounceAmount, player.velocity.z);
-        Instantiate(deathParticles, transform.position, transform.rotation);
-        Destroy(gameObject);
+        Instantiate(deathParticles, enemyAnim.transform.position, enemyAnim.transform.rotation);
+        enemyAnim.SetTrigger("Die");
+        centerAnim.SetTrigger("Stop");
+        Destroy(colliders.gameObject);
+        //Destroy(gameObject);
+
         
     }
 }
